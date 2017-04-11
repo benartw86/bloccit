0ecd50c5f36fdaac5900557bc6c0b7f00a5c7bdd
Ben <benartw86@yahoo.com>
2017-04-11 16:03:48 +0000

    Checkpoint 16, ORM

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ActiveRecord::Base
-    has_may :comments           #has_many method allows a post instance to have many comments related to it, allows allows retrieval of comments from a post
+    has_many :comments           #has_many method allows a post instance to have many comments related to it, allows allows retrieval of comments from a post
 end
