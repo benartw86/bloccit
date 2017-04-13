88410c4a7a47dc237097e46807f583f67f0f8a78
Ben <benartw86@yahoo.com>
2017-04-13 01:26:59 +0000

    Checkpoint 17 done, successful seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,33 @@
+require 'random_data'
+
+50.times do
+# create posts
+
+  Post.create!(
+    
+    title: RandomData.random_sentence,      #RandomData will create random stings for text and body
+    body: RandomData.random_paragraph
+)
+
+end
+
+posts = Post.all
+
+# create comments
+
+100.times do
+
+  Comment.create!(    #using the "!" (bang) instructs method to raise an error if there is a problem
+      
+    post: posts.sample,                 #sample returns a random element from the array, in order to pick a random post for each comment
+    body: RandomData.random_paragraph
+)
+
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
---
+++ lib/random_data.rb
@@ -0,0 +1,28 @@
+
+module RandomData             #RandomData is a module because its a standalone library, no dependencies/inheritance
+                              #the use of a module helps keep common functions organized and reusable 
+  def self.random_paragraph   
+    sentences = []            #We set sentences to an array. We create four to six random sentences and append them to sentences to create paragraph
+    rand(4..6).times do
+      sentences << random_sentence  
+    end
+    
+    sentences.join("")
+  end
+  
+  def self.random_sentence    #create sentences with random number of words (3 to 8).
+    strings = []
+    rand(3..8).times do
+      strings << random_word
+    end    
+  
+  sentence = strings.join(" ")
+  sentence.capitalize << "."     #append period to end of sentence
+  end
+  
+  def self.random_word
+    letters = ("a".."z").to_a  #
+    letters.shuffle!           #call shuffle on letters in place with "!", otherwise it would return an array
+    letters[0,rand(3..8)].join #We  join the zeroth through nth item in letters. The nth item is the result of  rand(3..8) which returns a random number greater than or equal to three and less than eight.
+  end
+end
