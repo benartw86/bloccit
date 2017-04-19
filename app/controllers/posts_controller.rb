b2eff57f0b875c728d45a3f35d1ba9caf739a46b
Ben <benartw86@yahoo.com>
2017-04-19 22:16:48 +0000

    assignment_18_intro_to_CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all  #we declare an instance variable @posts and assign it a collection of Post objects using the all method provided by ActiveRecord. all returns a collection of  Post objects.
+    @posts.each_with_index do |post, i|  
+      if i % 5 == 0 
+        post.title ='SPAM' 
+      end 
+    end
   end
 
   def show
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+    
+#  def censor(collection)
+#    collection.each_with_index do |post, i|  
+#      if i % 5 == 0 
+#        post.title ='SPAM' 
+#      end 
+#    end  
+#  end
+
 end
