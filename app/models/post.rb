214aebe85fbe3d08fb25a343d46d3158a4e38bbe
Ben <benartw86@yahoo.com>
2017-05-05 22:53:51 +0000

    Checkpoint 22, Rails validation, complete!

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ActiveRecord::Base
     
     belongs_to :topic
     has_many :comments, dependent: :destroy          #has_many method allows a post instance to have many comments related to it, allows allows retrieval of comments from a post
+
+    validates :title, length: { minimum: 5 }, presence: true
+    validates :body, length: { minimum: 20 }, presence: true
+    validates :topic, presence: true
 end
 
 
