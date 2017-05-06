05b50f08cf90dc22bc4b57c2e5baaf30369acc8e
Ben <benartw86@yahoo.com>
2017-05-06 15:59:09 +0000

    Checkpoint 23, Rails User Model Done!

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+      #we register an inline callback directly after the before_save callback
+    before_save { self.email = email.downcase if email.present? }
+    
+    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+    
+    validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"  
+    #first validation executes this to ensure that when we create a new user, they have a valid password
+    validates :password, length: { minimum: 6 }, allow_blank: true
+    
+    validates :email,
+              presence: true,
+              uniqueness: { case_sensitive: false }, 
+              length: { minimum: 3, maximum: 254 }
+              
+    has_secure_password
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :posts, except: [:index]
  end
 
+  # we create routes for new and create actions. The only hash key will prevent Rails from creating unnecessary routes.
+  resources :users, only: [:new, :create]
  
  get 'about' => 'welcome#about'  
  
