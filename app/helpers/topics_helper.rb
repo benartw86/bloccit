fbe5156afc20d89c04a3046463f406909931d59f
Ben <benartw86@yahoo.com>
2017-05-21 19:43:08 +0000

    Need to alter controller code to bar moderator from certain views

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -2,4 +2,10 @@ module TopicsHelper
   def user_is_authorized_for_topics?
       current_user && current_user.admin?
   end
+  
+  def user_is_authorized_for_edit_topics?
+    current_user && (current_user.admin? || current_user.moderator?)  
+  end
 end
+
+  
